3e159a9879c40f9b6ccca98ad25727d7b66bb09d
U-peke2-7\peke2 <peke2@peke2-7.(none)>
2012-12-31 14:44:43 +0900

    webページ上から呼び出すためのswfファイルが出来た。

---
+++ hello_mixi/flash.js
@@ -0,0 +1,11 @@
+//	Flashオブジェクトの埋め込み
+
+var	params =
+{
+	width : 640,
+	height : 480
+};
+
+gadgets.flash.embedFlash("http://sub0000498489.hmk-temp.com/test/hello_mixi/swf/HelloWorld.swf", document.getElementById("swf-here"), 9, params);
+
+
