13a310529a01444145088f5661d6a8efe8fe7a37
U-peke2-7\peke2 <peke2@peke2-7.(none)>
2012-12-31 11:30:29 +0900

    基本構成を作成。

---
+++ hello_mixi/hello.js
@@ -0,0 +1,13 @@
+function init() {
+  var req = opensocial.newDataRequest();
+  req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER), "viewer");
+  req.send(function(data) {
+    var viewer = data.get("viewer").getData();
+    var name = viewer.getDisplayName();
+    //document.getElementById("target").value = name;
+	var element = document.getElementById("disp");
+	var	text = element.innerHTML;
+	element.innerHTML = "<font size='6'>Hello "+ name + "!</font>";
+  });
+}
+gadgets.util.registerOnLoadHandler(init);
---
+++ mixiアプリテスト予定.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1356920340639" ID="ID_1800784459" MODIFIED="1356920851962" TEXT="mixi&#x30a2;&#x30d7;&#x30ea;&#x30c6;&#x30b9;&#x30c8;&#x4e88;&#x5b9a;">
+<node CREATED="1356920355969" ID="ID_1389282853" MODIFIED="1356920367989" POSITION="right" TEXT="hello world&#x4f5c;&#x6210;"/>
+<node CREATED="1356920368249" ID="ID_1934499505" MODIFIED="1356920393729" POSITION="right" TEXT="hello world(Flash&#x30a2;&#x30d7;&#x30ea;)&#x4f5c;&#x6210;">
+<node CREATED="1356920395707" ID="ID_1712129724" MODIFIED="1356920412659" TEXT="web&#x30da;&#x30fc;&#x30b8;&#x30d8;&#x7d44;&#x307f;&#x8fbc;&#x307f;"/>
+<node CREATED="1356920428357" ID="ID_1181229966" MODIFIED="1356920440359" TEXT="JavaScript&#x3068;&#x306e;&#x9023;&#x643a;">
+<node CREATED="1356920441447" ID="ID_1945781312" MODIFIED="1356920448389" TEXT="&#x30d5;&#x30ec;&#x30f3;&#x30c9;&#x306e;&#x53d6;&#x5f97;"/>
+</node>
+</node>
+</node>
+</map>
